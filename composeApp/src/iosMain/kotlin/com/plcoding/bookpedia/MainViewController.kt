e14d395f509056b93c6ab251097e709585eb7eab
Bzahov98 <bzahov1998@gmail.com>
2024-12-23 15:58:17 +0200

    Remove latest code

--- composeApp/src/iosMain/kotlin/com/plcoding/bookpedia/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/plcoding/bookpedia/MainViewController.kt
@@ -4,8 +4,8 @@ import androidx.compose.ui.window.ComposeUIViewController
 import com.plcoding.bookpedia.app.App
 import com.plcoding.bookpedia.di.initKoin
 
-fun MainViewController() = ComposeUIViewController(
-    configure = {
-        initKoin()
-    }
-) { App() }
+//fun MainViewController() = ComposeUIViewController(
+//    configure = {
+//        initKoin()
+//    }
+//) { App() }
